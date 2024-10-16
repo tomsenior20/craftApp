479ebfc63c12c0ae2e6006b90c29789de6ee64c7
tomsenior20 <tomsenior20@gmail.com>
2024-10-16 21:30:27 +0100

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // Enables static export
     reactStrictMode: true,
     images: {
-        unoptimized: true,  // Useful for handling static exports with images
+        unoptimized: true,
     },
     sassOptions: {
-        // Optionally add custom paths for SCSS
         includePaths: ['./styling'],
+    },
+    experimental: {
+        appDir: true,
     },
 };
 
