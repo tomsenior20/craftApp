6b3c62134cc4d508247662326d3353513479a87a
tomsenior20 <tomsenior20@gmail.com>
2024-10-21 17:23:22 +0100

    Your commit message

--- jest.setup.ts
+++
@@ -1,9 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-const { window } = new JSDOM();
-
-// Type casting to avoid TypeScript errors
-(global as any).window = window;
-(global as any).document = window.document;
-(global as any).navigator = window.navigator;
-(global as any).localStorage = window.localStorage;
