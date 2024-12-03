dd038914bf597725d5143391c102c7b200c93a3e
Tom Senior <thomas.senior@worldline.com>
2024-12-03 16:47:07 +0000

    base url test

--- app/components/api.tsx
+++ app/components/api.tsx
@@ -5,7 +5,10 @@ import { METHODS } from 'http';
 import { json } from 'stream/consumers';
 
 const PortNumber: string = process.env.PORT ?? '3010';
-const BASE_URL = process.env.NEXT_PUBLIC_APP || `http://localhost:${PortNumber}`;
+const BASE_URL = process.env.NEXT_PUBLIC_APP 
+  ? `http://${process.env.NEXT_PUBLIC_APP}:${PortNumber}` 
+  : `http://localhost:${PortNumber}`;
+  
 console.log("BASE_URL", BASE_URL);
 
 type Ticket = {
