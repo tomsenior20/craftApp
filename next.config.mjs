1dc2fb21c56f0b3f4d541fb8dcfede988db71513
Tom Senior <thomas.senior@worldline.com>
2024-12-11 15:24:00 +0000

    atered yml compose

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     swcMinify: true,
     serverRuntimeConfig: {
       NEXT_PUBLIC_FRONTEND_PORT: process.env.NEXT_PUBLIC_FRONTEND_PORT || 3000, // Default to 3000 if no PORT variable is set
+      NEXT_PUBLIC_BACKEND_PORT: process.env.NEXT_PUBLIC_BACKEND_PORT,
+      NEXT_PUBLIC_APP: process.env.NEXT_PUBLIC_APP,
+      SQLLite_DB_PATH: process.env.SQLLite_DB_PATH,
     },
     experimental: {
         forceSwcTransforms: true,
