033c286c0debeff38e86b0b7005722c86ebc3be7
Tom Senior <thomas.senior@worldline.com>
2024-12-11 15:36:52 +0000

    remove url

--- app/components/api.tsx
+++ app/components/api.tsx
@@ -5,7 +5,7 @@ import { json } from 'stream/consumers';
 
 const PortNumber = process.env.NEXT_PUBLIC_BACKEND_PORT;
 const BASE_URL = process.env.NEXT_PUBLIC_APP 
-  ? `http://${process.env.NEXT_PUBLIC_APP}:${PortNumber}` 
+  ? `${process.env.NEXT_PUBLIC_APP}:${PortNumber}` 
   : `http://localhost:${PortNumber}`;
 
 
