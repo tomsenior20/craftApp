ddfb02ff83a0c168a1e42b55640b868d0a6e279d
Tom Senior <thomas.senior@worldline.com>
2024-12-12 15:04:37 +0000

    updated image names

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styling/globals.scss";
-import Navbar from './components/nav';
 
 const inter = Inter({ subsets: ["latin"] });
 
